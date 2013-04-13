2be1a28bb0fc54ecf1c3a9d5ed1ebe7d049a3f0d
fyko chan <no-reply@no-mail.com>
2013-04-13 15:41:42 +0200

    Classes for storage and searching

---
+++ src/com/hiof/stud/localstories/Event.java
@@ -0,0 +1,40 @@
+package com.hiof.stud.localstories;
+
+public class Event {
+	public String text;
+	public int[] pictures;
+	//Year
+	public int day=0;
+	public int month=0;
+	public int year=0;
+	//Location [Posibly temp]
+	private double x=0;
+	private double y=0;
+	//
+	public String[] keywords;
+	
+	public Event(String txt, String date, int[] pics, String keys){
+		text=txt;
+		pictures=pics;
+		String[] dte=date.split("[.]");
+		if(dte.length==3){
+			day = Integer.parseInt(dte[0]);
+			month = Integer.parseInt(dte[1]);
+			year =  Integer.parseInt(dte[2]);
+		}
+		keywords = keys.split(",");
+	}
+
+	public double getX(){
+		return x;
+	}
+	public double getY(){
+		return y;
+	}
+	public void setX(double xx){
+		x=xx;
+	}
+	public void setY(double yy){
+		y=yy;
+	}
+}
---
+++ src/com/hiof/stud/localstories/Load.java
@@ -0,0 +1,29 @@
+package com.hiof.stud.localstories;
+
+public class Load {
+	private static String text;
+	private static int[] pictures;
+	private static String date;
+	private static String keywords;
+	private static Event evt;
+	public static void load(){
+		Library.emptyList();
+		//Event 1
+		text="Hello World";
+		date = "09.04.2013";
+		keywords = "Hello,World,day,thing,programing";
+		evt = new Event(text,date,pictures,keywords);
+		evt.setX(100.0);
+		evt.setY(100.0);
+		Library.addEvent(evt);
+		//Event 2
+		text="Goodbye World";
+		date = "21.12.2012";
+		keywords = "End, of,the,day,thing,programing";
+		evt = new Event(text,date,pictures,keywords);
+		evt.setX(-100.0);
+		evt.setY(-100.0);
+		Library.addEvent(evt);
+	}
+	
+}
