bb5d783e5a326fad0052502815a7bf8531b173ca
Eivind Arvesen <eivind.arvesen@gmail.com>
2013-04-16 13:16:33 +0200

    Added basic OpenStreetMap-support.

--- src/no/hiof/stud/localstories/MainActivity.java
+++ src/no/hiof/stud/localstories/MainActivity.java
@@ -1,7 +1,15 @@
 package no.hiof.stud.localstories;
 
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapController;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,11 +17,24 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "no.hiof.stud.localstories.MESSAGE";
+	
+	private MapView         mMapView;
+    private MapController   mMapController;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// osm-code here
+		mMapView = (MapView) findViewById(R.id.mapview);
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+        mMapView.setBuiltInZoomControls(true);
+        mMapController = mMapView.getController();
+        mMapController.setZoom(13);
+        GeoPoint gPt = new GeoPoint(51500000, -150000);
+        //Centre map near to Hyde Park Corner, London
+        mMapController.setCenter(gPt);
 	}
 
 	@Override
