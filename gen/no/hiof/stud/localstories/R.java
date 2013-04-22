3a93cdb4f8884a9a7dea9441c7f70ab4329a91ca
fyko chan <no-reply@no-mail.com>
2013-04-22 10:41:26 +0200

    more result design

--- gen/no/hiof/stud/localstories/R.java
+++ gen/no/hiof/stud/localstories/R.java
@@ -28,7 +28,8 @@ public final class R {
     public static final class id {
         public static final int EventText=0x7f080000;
         public static final int RadiusSeekBar=0x7f080008;
-        public static final int action_settings=0x7f080012;
+        public static final int action_settings=0x7f080013;
+        public static final int date=0x7f08000f;
         public static final int edit_message=0x7f080001;
         public static final int fragment_container=0x7f08000b;
         public static final int fromYearText=0x7f080003;
@@ -37,9 +38,9 @@ public final class R {
         public static final int icon=0x7f08000d;
         public static final int label=0x7f08000e;
         public static final int listView1=0x7f08000c;
-        public static final int map_zoom_controls=0x7f080010;
-        public static final int mapview=0x7f08000f;
-        public static final int menu_settings=0x7f080011;
+        public static final int map_zoom_controls=0x7f080011;
+        public static final int mapview=0x7f080010;
+        public static final int menu_settings=0x7f080012;
         public static final int radiusText=0x7f080009;
         public static final int radiusValue=0x7f08000a;
         public static final int range=0x7f080002;
