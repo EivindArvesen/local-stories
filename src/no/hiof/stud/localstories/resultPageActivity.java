7dca8d2903a7504fef3b281b752a6a0208a293d4
Eivind Arvesen <eivind.arvesen@gmail.com>
2013-04-27 20:43:33 +0200

    Revert "no message"
    This reverts commit 10e105e9bcbe71a329afe97443d1ea1f0c553b84.

--- src/no/hiof/stud/localstories/resultPageActivity.java
+++ src/no/hiof/stud/localstories/resultPageActivity.java
@@ -60,7 +60,6 @@ public class ResultPageActivity extends Activity {
 		    		R.layout.item, R.id.label, values, events);
 		    listview.setAdapter(adapter);
 
-		    
 		    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 		      @Override
@@ -101,7 +100,15 @@ public class ResultPageActivity extends Activity {
 		      return true;
 		    }
 		    
-		    
+		    //Unsure if/how this works, needs testing
+		    public View getView(int position, View convertView, ViewGroup viewGroup) {
+		        Event event = events.get(position);
+		        TextView tvContact = (TextView) convertView.findViewById(R.id.date);
+		        tvContact.setText(event.day + "/" + event.month+ " " +event.year);
+
+		        
+		        return convertView;
+		    }
 
 		  }
 
