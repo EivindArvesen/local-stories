8bf4beef02043370a2e2500b3670f70653a1e8da
Eivind Arvesen <eivind.arvesen@gmail.com>
2013-04-27 20:56:11 +0200

    Had to revert because of particularly nasty merge-conflicts. Manually added changes from reverted commits (Test event 1 and 2 + small change in event layout). IMPORTANT: Remember to use commit-messages, and to push and pull often in the future!

--- gen/no/hiof/stud/localstories/R.java
+++ gen/no/hiof/stud/localstories/R.java
@@ -26,30 +26,31 @@ public final class R {
         public static final int seek_thumb_pressed=0x7f020002;
     }
     public static final class id {
-        public static final int EventText=0x7f080000;
-        public static final int RadiusSeekBar=0x7f080008;
-        public static final int action_settings=0x7f080017;
-        public static final int button1=0x7f08000b;
-        public static final int button2=0x7f08000c;
-        public static final int button3=0x7f08000e;
-        public static final int button4=0x7f08000f;
-        public static final int date=0x7f080012;
-        public static final int edit_message=0x7f080001;
-        public static final int fragment_container=0x7f08000d;
-        public static final int fromYearText=0x7f080003;
-        public static final int fromYearValue=0x7f080004;
-        public static final int hidden_value=0x7f080005;
-        public static final int label=0x7f080011;
-        public static final int listView1=0x7f080013;
-        public static final int map_zoom_controls=0x7f080015;
-        public static final int mapview=0x7f080014;
-        public static final int menu_settings=0x7f080016;
-        public static final int picture=0x7f080010;
-        public static final int radiusText=0x7f080009;
-        public static final int radiusValue=0x7f08000a;
-        public static final int range=0x7f080002;
-        public static final int toDateText=0x7f080006;
-        public static final int toYearValue=0x7f080007;
+        public static final int EventText=0x7f080001;
+        public static final int RadiusSeekBar=0x7f080009;
+        public static final int action_settings=0x7f080018;
+        public static final int button1=0x7f08000c;
+        public static final int button2=0x7f08000d;
+        public static final int button3=0x7f08000f;
+        public static final int button4=0x7f080010;
+        public static final int date=0x7f080013;
+        public static final int edit_message=0x7f080002;
+        public static final int fragment_container=0x7f08000e;
+        public static final int fromYearText=0x7f080004;
+        public static final int fromYearValue=0x7f080005;
+        public static final int hidden_value=0x7f080006;
+        public static final int label=0x7f080012;
+        public static final int listView1=0x7f080014;
+        public static final int map_zoom_controls=0x7f080016;
+        public static final int mapview=0x7f080015;
+        public static final int menu_settings=0x7f080017;
+        public static final int picture=0x7f080011;
+        public static final int radiusText=0x7f08000a;
+        public static final int radiusValue=0x7f08000b;
+        public static final int range=0x7f080003;
+        public static final int scrollView1=0x7f080000;
+        public static final int toDateText=0x7f080007;
+        public static final int toYearValue=0x7f080008;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
