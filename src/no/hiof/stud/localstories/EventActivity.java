d84eaf4df3bdd36bda19532ddf29e70104dc42aa
Eivind Arvesen <eivind.arvesen@gmail.com>
2013-04-29 01:08:09 +0200

    Fixed issue #3: Event-Layout is complete. Missing "show on map"-functionality.

--- src/no/hiof/stud/localstories/EventActivity.java
+++ src/no/hiof/stud/localstories/EventActivity.java
@@ -3,7 +3,9 @@ package no.hiof.stud.localstories;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class EventActivity extends Activity {
@@ -17,6 +19,9 @@ public class EventActivity extends Activity {
 	    String message = intent.getStringExtra(resultPageActivity.EVENT_ID);
 	    int id= Integer.parseInt(message);
 	    Event event= Library.Events.get(id);
+	    
+	    TextView EventHeader = (TextView) findViewById(R.id.EventHeader);
+	    EventHeader.setText(event.header);
 	    TextView EventText = (TextView) findViewById(R.id.EventText);
         EventText.setText(event.text);
 	}
@@ -26,6 +31,11 @@ public class EventActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_event, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Back button */
+	public void backButton(View view) {
+		finish();
 	}
 
 }
